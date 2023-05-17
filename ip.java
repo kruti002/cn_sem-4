7aad8aef5303f7efcab0a794f10e834d31858e5c
kruti shah <81468804+kruti002@users.noreply.github.com>
2023-05-17 22:13:45 +0530

    Update ip.java

--- ip.java
+++ ip.java
@@ -1,42 +1,49 @@
-
 import java.util.*;
-
+import java.io.*;
 public class Main{
- public static void main(String[] args) {
- Scanner sc =new Scanner(System.in);
- System.out.println("Enter IP Address: ");
- String ip =sc.next();
- String checkclass = ip.substring(0,3);
- int cc = Integer.parseInt(checkclass);
- String mask = null;
- if(cc>0 && cc<224){
- if(cc<=127){
- System.out.println("IP Address is of Class A");
- mask="255.0.0.0";
- }
- if(cc>=128 && cc<=191){
- System.out.println("IP Address is of Class B");
- mask="255.255.0.0";
- }
- if(cc>=192 && cc<=223){
- System.out.println("IP Address is of Class C");
- mask="255.255.255.0";
- }
- if(cc>=224){
- System.out.println("IP Address is used for Multicasting or reserved");
- }
- }
- System.out.println("Subnet Mask:\n"+mask);
- String networkAddr="";
- String[] ipAddrParts = ip.split("\\.");
- String[] maskParts = mask.split("\\.");
- for(int i=0;i<=3;i++){
- int x = Integer.parseInt(ipAddrParts[i]);
- int y = Integer.parseInt(maskParts[i]);
- int z = x & y;
- 
- networkAddr += z+".";
- }
- System.out.println("SUBNET Address:\n"+networkAddr);
- }
+    public static void main(String[] args)throws IOException{
+        System.out.println("Ip Address Matching");
+        System.out.println("Enter the IP Address : ");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String[] index = input.split("\\.");
+        int check = Integer.parseInt(index[0]);
+        String mask=null;
+        if(check>= 0 && check<256){
+            if(check < 128){
+                System.out.println("The IP Address Is of Class A");
+                mask = "255.0.0.0";
+                System.out.println("Subnet Mask : "+mask);
+            }
+            else if(check <192){
+                System.out.println("The IP Address Is of Class B");
+                mask = "255.255.0.0";
+                System.out.println("Subnet Mask : "+mask);
+            }
+            else if(check < 224){
+                System.out.println("The IP Address Is of Class C");
+                mask = "255.255.255.0";
+                System.out.println("Subnet Mask : "+mask);
+            }
+            else if(check < 240){
+                System.out.println("The IP Address Is of Class D");
+            }
+            else if(check < 256){
+                System.out.println("The IP Address Is of Class E");
+            }
+        }
+        else{
+            System.out.println("Enter a valid IP address");
+        }
+        String NetworkAddress="";
+        String[] masking = mask.split("\\.");
+        for(int i=0;i<4;i++){
+            int x = Integer.parseInt(index[i]);
+            int y = Integer.parseInt(masking[i]);
+            int z = x & y;
+            NetworkAddress = NetworkAddress + z + '.';
+            
+        }
+        System.out.println(NetworkAddress);
+    }
 }
